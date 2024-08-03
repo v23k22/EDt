f33db74fca81242d35e4cc6f6890c7488ff8e0b5
v23k22 <95199578+v23k22@users.noreply.github.com>
2024-08-03 10:52:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const พร็อกซีไอพีs = ['ipdb.rr.nu', 'hk.cf.zhetengsha.eu.org', 'hk.ipdb.rr.nu', 'proxyip.aliyun.fxxk.dedyn.io', 'proxyip.oracle.fxxk.dedyn.io'];
+const พร็อกซีไอพีs = ['my-telegram-is-herocore.onecf.eu.org', 'hk.cf.zhetengsha.eu.org', 'bpb.radically.pro', 'proxyip.aliyun.fxxk.dedyn.io', 'proxyip.oracle.fxxk.dedyn.io'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
